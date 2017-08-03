f20a6d5302ec57993265b25ce90ef53d453e9277
Adam M Montano <amontano495@gmail.com>
2017-08-03 13:48:47 -0700

    modified:   haskellWiki.hs

--- haskellWiki.hs
+++ haskellWiki.hs
@@ -8,3 +8,9 @@ cons8_end (x:xs) = (x:xs) ++ 8:[]
 
 myCons :: [a] -> a -> [a]
 myCons (x:xs) q = q:(x:xs)
+
+listToTup :: [a] -> (a,[a])
+listToTup (x:xs) = (x,xs)
+
+grab5th :: [a] -> a
+grab5th (x:xs) = head ( tail ( tail ( tail ( xs ) ) ) )
